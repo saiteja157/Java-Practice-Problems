c206a11ddc100ecb484640d9965d217274d3858c
Teja <saitejapaladi03@gmail.com>
2025-10-10 22:48:11 +0530

    Update firsthightvalue.java

--- Array/firsthightvalue.java
+++ Array/firsthightvalue.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+        
+        int high = Integer.MIN_VALUE;
+    
+        for(int i=0; i<n; i++){
+            if(a[i]>high){
+                
+                high = a[i];
+            }
+           
+        }
+        System.out.print(high);
+    }
+}
